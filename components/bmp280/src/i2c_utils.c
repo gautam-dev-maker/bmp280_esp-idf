f969b8b233558af5401f446846872df67c869ccf
laukik-hase <laukik17@gmail.com>
2020-11-08 10:39:26 +0530

    Fixed i2c_write_to_slave

--- components/bmp280/src/i2c_utils.c
+++ components/bmp280/src/i2c_utils.c
@@ -24,6 +24,7 @@ esp_err_t i2c_write_to_slave(uint8_t slave_addr, uint8_t reg_addr, size_t len, u
     i2c_cmd_handle_t cmd = i2c_cmd_link_create();
     i2c_master_start(cmd);
     i2c_master_write_byte(cmd, (slave_addr << 1) | WRITE_BIT, ACK_CHECK_EN);
+    i2c_master_write_byte(cmd, reg_addr, ACK_CHECK_EN);
     i2c_master_write(cmd, data_wr, len, ACK_CHECK_EN);
     i2c_master_stop(cmd);
     esp_err_t ret = i2c_master_cmd_begin(I2C_MASTER_NUM, cmd, 1000 / portTICK_RATE_MS);
